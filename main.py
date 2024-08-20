292addf6dfea540292fa269862e522b00e3896c5
dwseoh <dongwanseoh@gmail.com>
2024-08-20 19:53:47 -0400

    .

--- main.py
+++ main.py
@@ -17,6 +17,43 @@ categories = {
 }
 
 
+def calculateMark():
+    category_marks = []
+    for k,v in categories.items():
+        category_marks.append([0.0,0.0,0.0,v,k])
+    
+    for dataset in data:
+       category_marks[dataset[2]][0] += (dataset[0]*dataset[1])
+       category_marks[dataset[2]][1] += dataset[1]
+       category_marks[dataset[2]][2] = category_marks[dataset[2]][0] / category_marks[dataset[2]][1]
+
+    totalMark = 0
+    i = 0
+    for dataset in category_marks:
+        if dataset[1] != 0:
+            totalMark += (dataset[3] * dataset[2] / 100)
+            i += (dataset[3]/100)
+        else:
+            pass
+    totalMark /= i
+
+    return category_marks,totalMark
+
+    
+
+
+def addEntry():
+    print("\nADDING ENTRY:")
+
+def printMarkReport(raw,mark):
+    print(f"\nMARK REPORT\nFinal Mark: {mark:.2f}%\nCategory Breakdown:")
+    for data in raw: 
+        if data[1] != 0:
+            print(f" - {data[4]:<15} ( {data[3]:<3}% )  -  {data[2]:<3.2f} %")
+        else:
+            print(f" - {data[4]:<15} ( {data[3]:<3}% )  -  N/A (no entry)")
+
+
 def printCategories():
     print("\nCATEGORIES:")
     c = 1
@@ -46,17 +83,22 @@ if not categories:
 
 userInput = ""
 
+#add an option to edit categories later
+
 while userInput != "Exit":
     userInput = input("What do you want to do?\n\n1 - Add an entry\n2 - Remove an entry\n3 - Reset all data\n4 - Mark report\n5 - View Categories\nInput: ")
     
     if userInput == '1':
-        pass
+        addEntry()
     elif userInput == '2':
         pass
     elif userInput == '3':
         pass
     elif userInput == '4':
-        pass
+        a,b=calculateMark()
+        print(a,b)
+        printMarkReport(a,b)
+        
     elif userInput == '5':
         printCategories()
     
